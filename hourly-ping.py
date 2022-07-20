1f81cb94cb64ffce6e2ec7845dfef6627200417b
Patrick van Staveren <trick@vanstaveren.us>
2022-07-20 05:23:27 +0100

    huge refactor ibto shared modem class and finallt fix httpterm behavior

--- hourly-ping.py
+++ hourly-ping.py
@@ -1,70 +1,16 @@
 #!/usr/bin/env python
 
-import serial
 import RPi.GPIO as GPIO     
 import sys
 import time
+import modem as modem_module
 
-power_cycle = True
-
-if power_cycle:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(4, GPIO.OUT)
-    #GPIO.output(4, GPIO.LOW) # Turn modem off
-    #time.sleep(3) # wait a sec for it to turn off
-
-    # Now turn it on (pulse high, wait for it, then sustain high)
-    GPIO.output(4, GPIO.HIGH)
-    time.sleep(0.5)
-    GPIO.output(4, GPIO.LOW)
-    time.sleep(1)
-    GPIO.output(4, GPIO.HIGH)
-    time.sleep(3)
-
-# Enable Serial Communication
-port = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=1)
-#received = port.read(1000)
-#print(received.decode('latin1'))
-#print("\n")
-
-# Transmitting AT Commands to the Modem
-# '\r\n' indicates the Enter key
-
-def poweroff():
-    write('AT+CPOWD=1')
-    time.sleep(5)
-    print(port.read(1000))
-    GPIO.output(4, GPIO.LOW) # Turn modem power switch off
-    sys.exit(1)
-
-def write(command):
-    command = command + "\r\n"
-    command = command.encode('latin1')
-    port.write(command)
-
-def read_ok():
-    received = b''
-    while True:
-        received += port.read(1000)
-        if len(received) > 0:
-            if received.decode('latin1').find("ERROR") >= 0:
-                print(received)
-                poweroff()
-
-            if received.decode('latin1').find("OK") >= 0:
-                print(received)
-                break
-        print(".", end="", flush=True)
-        time.sleep(0.1)
-
-def write_ok(command):
-    write(command)
-    read_ok()
-
+modem = modem_module.modem()
+modem.init()
 
 while True:
-    write('AT')
-    rcv = port.read(100)
+    modem.write_noblock('AT')
+    rcv = modem.port.read(100)
     if len(rcv) > 0:
         print(rcv)
         break
@@ -76,43 +22,38 @@ while True:
 #write_ok('AT+SAPBR=0,1') # Close GPRS context # not idempotent...
 #write_ok('AT+HTTPTERM') # Close HTTP handler in case it was left open # not idempotent...
 
-print("Module online, giving 15 more seconds to connect:")
-time.sleep(15)
-rcv = port.read(100)
-print(rcv)
+#if power_cycle:
+#    print("Module online, giving 15 more seconds to connect:")
+#    time.sleep(15)
+#    rcv = port.read(100)
+#    print(rcv)
 
 # check status
 print("Network signal quality query, returns a signal value:")
-write_ok('AT+CSQ')
+modem.write_ok('AT+CSQ')
 #print("Firmware version:")
 #write_ok('AT+CGMR')
 print("Network registration status:")
-write_ok("AT+CREG?")
+modem.write_ok("AT+CREG?")
 print("GPRS attachment status:")
-write_ok("AT+CGATT?")
+modem.write_ok("AT+CGATT?")
 
 
 
 # Connect to GPRS:
-write_ok('AT+CSTT="hologram"')
-write_ok('AT+CIICR')
-write_ok('AT+SAPBR=3,1,"Contype","GPRS"')
-write_ok('AT+SAPBR=3,1,"APN","hologram"')
-write_ok('AT+SAPBR=1,1') # open GPRS context
-write_ok('AT+SAPBR=2,1') # Query GPRS context
+modem.write_ok('AT+CSTT="hologram"')
+modem.write_ok('AT+CIICR')
+modem.write_ok('AT+SAPBR=3,1,"Contype","GPRS"')
+modem.write_ok('AT+SAPBR=3,1,"APN","hologram"')
+modem.write_ok('AT+SAPBR=1,1') # open GPRS context
+modem.write_ok('AT+SAPBR=2,1') # Query GPRS context
 
 
 # Now run the HTTP command:
-write_ok('AT+HTTPINIT')
-write_ok('AT+HTTPPARA="CID",1')
-write_ok('AT+HTTPPARA="URL","http://hacks.v9n.us/sim800c/"')
-write_ok('AT+HTTPACTION=0')
-write_ok('AT+HTTPREAD')
-# write_ok('AT+HTTPTERM') # seems to return an error even if we get a 200? weird
-# for now, just shut the module down now:
-poweroff()
-#write_ok('AT+SAPBR=0,1') # Close GPRS context
+modem.write_ok('AT+HTTPINIT')
+modem.write_ok('AT+HTTPPARA="CID",1')
+modem.write_ok('AT+HTTPPARA="URL","http://hacks.v9n.us/sim800c/"')
+modem.write_ok('AT+HTTPACTION=0')
+modem.write_ok('AT+HTTPREAD')
 
-# Now turn the modem off:
-
-#GPIO.output(4, GPIO.LOW) # Turn modem off
+modem.cleanup()
---
+++ modem/__init__.py
@@ -0,0 +1,80 @@
+import sys
+import time
+import serial
+import RPi.GPIO as GPIO
+
+class modem:
+
+    def init(self):
+        self.port = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=1)
+
+    def poweroff(self):
+        self.write('AT+CIPSTATUS')
+        self.write('AT+HTTPTERM')
+        self.write('AT+SAPBR=0,1')
+        self.write('AT+CIPSHUT')
+        self.write('AT+CIPSTATUS')
+        sys.exit(2)
+        self.write('AT+CPOWD=1')
+        time.sleep(5)
+        print(self.port.read(1000))
+        GPIO.output(4, GPIO.LOW) # Turn modem power switch off
+        sys.exit(1)
+
+    def write_noblock(self, command):
+        command = command + "\r\n"
+        command = command.encode('latin1')
+        self.port.write(command)
+
+    def write(self, command):
+        command = command + "\r\n"
+        command = command.encode('latin1')
+        self.port.write(command)
+        received = b''
+        while True:
+            received += self.port.read(1000)
+            if len(received) > 0:
+                break
+            print(".", end="", flush=True)
+            time.sleep(0.1)
+        print(received)
+
+    def read_ok(self):
+        self.read_expect("OK")
+
+    def read_expect(self, expect_message):
+        received = b''
+        while True:
+            received += self.port.read(1000)
+            if len(received) > 0:
+                if received.decode('latin1').find("ERROR") >= 0:
+                    print(received)
+                    self.poweroff()
+
+                if received.decode('latin1').find(expect_message) >= 0:
+                    print(received)
+                    break
+            print(".", end="", flush=True)
+            time.sleep(0.1)
+
+    def write_ok(self, command):
+        self.write_noblock(command)
+        self.read_ok()
+
+    def write_expect(self, command, expect):
+        self.write_noblock(command)
+        self.read_expect(expect)
+
+    def power_on(self, ):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(4, GPIO.OUT)
+        GPIO.output(4, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(4, GPIO.LOW)
+        time.sleep(1)
+    
+    def cleanup(self):
+        self.write("AT+HTTPTERM")
+        self.write('AT+SAPBR=0,1') # Close GPRS context
+        self.write("AT+CIPSHUT") # Close PDP thing
+        self.write("AT+HTTPTERM")
