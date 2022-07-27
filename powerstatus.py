aa09fd67b999f8d33a4074dcb8f49e7bffe12e0a
Patrick van Staveren <trick@vanstaveren.us>
2022-07-27 21:13:45 +0100

    utility commands for debugging

---
+++ powerstatus.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import serial
+import RPi.GPIO as GPIO     
+import sys
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+
+print("GPIO Function:")
+print(GPIO.gpio_function(4))
+print("GPIO State:")
+print(GPIO.input(4))
+
+
+# Enable Serial Communication
+port = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=1)
+#received = port.read(1000)
+#print(received.decode('latin1'))
+#print("\n")
+
+print("Buffer:")
+print(port.read(1000))
+print("Sending AT:")
+port.write("AT\r\n".encode('latin1'))
+time.sleep(1)
+print(port.read(1000))
+print("Sending AT:")
+port.write("AT\r\n".encode('latin1'))
+time.sleep(1)
+print(port.read(1000))
+print("Sending AT:")
+port.write("AT\r\n".encode('latin1'))
+time.sleep(1)
+print(port.read(1000))
