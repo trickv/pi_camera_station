be827dd16cedf7676d1736f5b62637f42c950f86
Patrick van Staveren <trick@vanstaveren.us>
2023-07-08 05:39:04 +0000

    disconnect after each beacon

--- beacon.py
+++ beacon.py
@@ -17,11 +17,11 @@ while True:
     print(".", end='', flush=True)
     time.sleep(1)
 
-#modem.lte_configure()
-#modem.lte_connect()
+modem.lte_configure()
+modem.lte_connect()
 modem.print_status()
 response = modem.lte_send_beacon()
-#modem.lte_disconnect()
+modem.lte_disconnect()
 
 if (response.find("ET_PHONE_HOME") > 0):
     print("ET PHONE HOME RECEIVED!")
