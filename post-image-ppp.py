5d6b40eb0f2f5e0d4fb5f14606584c5ccd3e5589
Patrick van Staveren <trick@vanstaveren.us>
2023-07-16 08:36:57 +0000

    post as binary and add some hacks and it works

--- post-image-ppp.py
+++ post-image-ppp.py
@@ -7,6 +7,7 @@ import os
 import time
 import datetime
 import modem as modem_module
+import pathlib
 
 modem = modem_module.modem()
 modem.init()
@@ -21,13 +22,17 @@ else:
     os.unlink("{}.png".format(image_file))
 
 url = "http://hacks.v9n.us/sim800c/?image=new"
+touchfile = '/dev/shm/now-image'
+pathlib.Path(touchfile).touch()
 
-proc = subprocess.Popen("sudo pppd call gprs", shell=True)
+proc = subprocess.Popen("sudo pppd call image", shell=True)
 print("gprs called")
 time.sleep(5)
 print("now curl")
-cmd = "curl -d @{}.webp {}".format(image_file, url)
-subprocess.run(cmd, shell=True)
+cmd = "curl --data-binary @{}.webp {}".format(image_file, url)
+print("cmd: {}".format(cmd))
+subprocess.run([cmd], shell=True)
+os.unlink(touchfile)
 print("cleam up in ten")
 time.sleep(10)
 proc.terminate()
