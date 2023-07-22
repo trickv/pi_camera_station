8092aeafabd29d8f3b25377eec6e1f3769228a6c
Patrick van Staveren <trick@vanstaveren.us>
2023-07-22 12:28:47 +0000

    stub for gps oneshot script

---
+++ gps.py
@@ -0,0 +1,7 @@
+# turn off, wait, turj on relay
+# press power button soft
+# turn oj gps power
+# poll for signal, once found, poll for 5m
+AT+CGNSINF
+# write to disk
+# power cycle, run startup...py
